bcf989b96e466b06fd930650b16dc80acffd3766
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-12-05 11:13:19 +0100

    linked in the js, css to the index.html

--- javascript.js
+++ javascript.js
@@ -3,3 +3,13 @@ let memory_values = [];
 let memory_tile_id = [];
 let tiles_flipped = 0;
 let output = '';
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+memory_array = shuffle(memory_array);
