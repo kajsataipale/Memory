d316f5e7ae6aa8f94435a03672cf109333739550
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2017-12-11 10:18:04 +0100

    Updated the README file, fixed the structure in the code and added some comments

--- javascript.js
+++ javascript.js
@@ -7,11 +7,11 @@ const board = document.querySelector('.memory-board');
 
 
 function shuffle(a) {    // This function takes the cardsImage and return a random image.
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
 
 cardsImage = shuffle(cardsImage);
@@ -19,69 +19,69 @@ cardsImage = shuffle(cardsImage);
 
 const boardDiv = document.querySelector('.memory-board'); // This function will generate all the cards in the board.
 
-   var arrayDiv = new Array();
-   for (var i = 0; i <cardsImage.length; i++) {
-       arrayDiv[i] = document.createElement('div');
-        arrayDiv[i].className = 'cards';
-        arrayDiv[i].dataset.id=cardsImage[i];
-        arrayDiv[i].innerHTML = `<span class="hidden" data-id="${cardsImage[i]}"></span>`;
-        arrayDiv[i].childNodes[0].style.backgroundImage = `url('images/${cardsImage[i]}.jpg')`;
+var arrayDiv = new Array();
+for (var i = 0; i <cardsImage.length; i++) {
+  arrayDiv[i] = document.createElement('div');
+  arrayDiv[i].className = 'cards';
+  arrayDiv[i].dataset.id=cardsImage[i];
+  arrayDiv[i].innerHTML = `<span class="hidden" data-id="${cardsImage[i]}"></span>`;
+  arrayDiv[i].childNodes[0].style.backgroundImage = `url('images/${cardsImage[i]}.jpg')`;
 
-         document.querySelector(".memory-board").appendChild(arrayDiv[i]);
-   }
-
-
-     const Flipcards = document.querySelectorAll('.cards'); // Here´s where I add a class to all the cards to make them flip.
+  document.querySelector(".memory-board").appendChild(arrayDiv[i]);
+}
 
 
+const Flipcards = document.querySelectorAll('.cards'); // Here´s where I add a class to all the cards to make them flip.
 
 
 
-     Array.from(Flipcards).forEach( (card) =>{
-       card.addEventListener('click', () => {
-         card.classList.toggle('card-flipped');
-          memory_values.push(card);
-         console.log(memory_values);
 
-         if (memory_values.length === 2)   {        // here I set so you only can press two cards at time
-           const frame = document.querySelector('.memory-board');
-           frame.classList.add('hold');
 
-         if (memory_values.length===2){
-           if (memory_values[0].dataset.id===memory_values[1].dataset.id) {
-             tiles_flipped++;
-             memory_values[0].classList.add('clicked');       // here I add a class so that the matched cards can´t be clicked
-             memory_values[1].classList.add('clicked');
-             console.log('Match');
-             console.log(tiles_flipped);
-             memory_values= [];
+Array.from(Flipcards).forEach( (card) =>{
+  card.addEventListener('click', () => {
+    card.classList.toggle('card-flipped');
+    memory_values.push(card);
+    console.log(memory_values);
+
+    if (memory_values.length === 2)   {        // here I set so you only can press two cards at time
+      const frame = document.querySelector('.memory-board');
+      frame.classList.add('hold');
+
+      if (memory_values.length===2){
+        if (memory_values[0].dataset.id===memory_values[1].dataset.id) {
+          tiles_flipped++;
+          memory_values[0].classList.add('clicked');       // here I add a class so that the matched cards can´t be clicked
+          memory_values[1].classList.add('clicked');
+          console.log('Match');
+          console.log(tiles_flipped);
+          memory_values= [];
+          frame.classList.remove('hold');
+        }
+        else  {
+
+
+          setTimeout(function () {
+            console.log('No match');
+
+            memory_values[0].classList.remove('card-flipped');        // if the cards don´t match the cards flip back after 1.5 sec.
+            memory_values[1].classList.remove('card-flipped');
             frame.classList.remove('hold');
-           }
-           else  {
-
-
-            setTimeout(function () {
-              console.log('No match');
-
-              memory_values[0].classList.remove('card-flipped');        // if the cards don´t match the cards flip back after 1.5 sec.
-              memory_values[1].classList.remove('card-flipped');
-              frame.classList.remove('hold');
 
             memory_values= [];
-            }, 1500)
-            }
+          }, 1500)
+        }
 
 
-           if (tiles_flipped===8) {
-             console.log('You win');
-             tiles_flipped=0;
-           }
+        if (tiles_flipped===8) {
+          console.log('You win');
+          tiles_flipped=0;
+        }
 
-         }
+      }
 
-         }
-       })
-     });
+    }
+  })
+});
 
 
 
@@ -91,20 +91,20 @@ const Refresh = document.querySelector('.reset'); // My reset function, when you
 Refresh.addEventListener('click', (a) => {
 
   function shuffle(a) {
-      for (let i = a.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [a[i], a[j]] = [a[j], a[i]];
-      }
-      return a;
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
   }
   cardsImage = shuffle(cardsImage);
 
- tiles_flipped=0;
+  tiles_flipped=0;
   for (var i = 0; i <cardsImage.length; i++) {
-       arrayDiv[i].classList.remove('card-flipped');
-       arrayDiv[i].dataset.id=cardsImage[i];
-       arrayDiv[i].innerHTML = `<span class="hidden" data-id="${cardsImage[i]}"></span>`;
-       arrayDiv[i].childNodes[0].style.backgroundImage = `url('images/${cardsImage[i]}.jpg')`;
+    arrayDiv[i].classList.remove('card-flipped');
+    arrayDiv[i].dataset.id=cardsImage[i];
+    arrayDiv[i].innerHTML = `<span class="hidden" data-id="${cardsImage[i]}"></span>`;
+    arrayDiv[i].childNodes[0].style.backgroundImage = `url('images/${cardsImage[i]}.jpg')`;
 
   }
-  })
+})
